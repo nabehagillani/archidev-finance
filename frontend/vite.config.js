d8a43ae1345c745681e57e3ed0c57221e9b52a1d
Nabeha Fatima <nabeha.gillani@gmail.com>
2026-09-08 11:09:22 +0500

    Add files via upload

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['recharts', 'react', 'react-dom'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
+  },
+})
